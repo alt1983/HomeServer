4d12449e7919dedf0fbfd157d358941c8a0d6b00
alt1983 <timofeev_a_v@mail.ru>
2022-09-18 18:52:06 +0300

    Commit 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,18 @@ public class Main {
     public static void main(String[] args) {
         final var server = new Server(List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js"));
         final int port = 9999;
+
+        // добавление handler'ов (обработчиков)
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                // TODO: handlers code
+            }
+        });
         server.listen(port);
     }
 }
